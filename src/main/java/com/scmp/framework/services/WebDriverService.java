cdf18255438f541d195d1a2eca8c0bd408a63860
RayZhou <ray.zhou@scmp.com>
2024-10-24 17:27:51 +0800

    Updated WebDriverService

--- src/main/java/com/scmp/framework/services/WebDriverService.java
+++ src/main/java/com/scmp/framework/services/WebDriverService.java
@@ -19,6 +19,9 @@ import java.time.Duration;
 
 import static com.scmp.framework.utils.Constants.*;
 
+/**
+ * WebDriverService - Manages the lifecycle of WebDriver instances for test execution.
+ */
 @Component
 public class WebDriverService {
 	private static final Logger frameworkLogger = LoggerFactory.getLogger(WebDriverService.class);
@@ -30,50 +33,64 @@ public class WebDriverService {
 		this.context = context;
 	}
 
+	/**
+	 * Get the current WebDriver instance.
+	 *
+	 * @return the current RemoteWebDriver instance
+	 */
 	public RemoteWebDriver getDriver() {
 		return remoteWebDriver.get();
 	}
 
+	/**
+	 * Set the current WebDriver instance.
+	 *
+	 * @param driver the RemoteWebDriver instance to set
+	 */
 	protected void setDriver(RemoteWebDriver driver) {
 		remoteWebDriver.set(driver);
 	}
 
-	public void startDriverInstance(MutableCapabilities browser, Dimension screenDimension)
-			throws Exception {
+	/**
+	 * Start a new WebDriver instance based on the provided browser capabilities and screen dimensions.
+	 *
+	 * @param browser         the browser capabilities (e.g., ChromeOptions, FirefoxOptions)
+	 * @param screenDimension the desired screen dimensions for the browser window
+	 * @throws Exception if an error occurs while starting the WebDriver instance
+	 */
+	public void startDriverInstance(MutableCapabilities browser, Dimension screenDimension) throws Exception {
 		RemoteWebDriver currentDriverSession;
 
 		// For Execution Mode
 		if (!context.isLocalExecutionMode()) {
-			currentDriverSession =
-					new RemoteWebDriver(new URL(context.getFrameworkConfigs().getHostUrl()), browser);
-		}
-		// For Debug Mode, launch local driver
-		else {
-
+			// Start a remote WebDriver session
+			currentDriverSession = new RemoteWebDriver(new URL(context.getFrameworkConfigs().getHostUrl()), browser);
+		} else {
+			// For Debug Mode, launch local driver
 			if (browser.getBrowserName().equals(Browser.CHROME.browserName())) {
-				frameworkLogger.info("Launch local Chrome Browser");
-				System.setProperty(
-						CHROME_DRIVER_SYSTEM_PROPERTY_NAME,
-						context.getGlobalVariables(CHROME_DRIVER_PATH).toString());
+				frameworkLogger.info("Launching local Chrome Browser");
+				System.setProperty(CHROME_DRIVER_SYSTEM_PROPERTY_NAME, context.getGlobalVariables(CHROME_DRIVER_PATH).toString());
 				currentDriverSession = new ChromeDriver((ChromeOptions) browser);
 			} else {
-				frameworkLogger.info("Launch local Firefox Browser");
-				System.setProperty(
-						FIREFOX_DRIVER_SYSTEM_PROPERTY_NAME,
-						context.getGlobalVariables(FIREFOX_DRIVER_PATH).toString());
+				frameworkLogger.info("Launching local Firefox Browser");
+				System.setProperty(FIREFOX_DRIVER_SYSTEM_PROPERTY_NAME, context.getGlobalVariables(FIREFOX_DRIVER_PATH).toString());
 				currentDriverSession = new FirefoxDriver((FirefoxOptions) browser);
 			}
 		}
 
+		// Set implicit wait timeout
 		currentDriverSession.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		this.setDriver(currentDriverSession);
 
-		// Set screen dimension
+		// Set screen dimensions
 		currentDriverSession.manage().window().setSize(screenDimension);
 	}
 
+	/**
+	 * Stop the current WebDriver instance.
+	 */
 	public void stopWebDriver() {
-		if (this.getDriver()!=null) {
+		if (this.getDriver() != null) {
 			this.getDriver().quit();
 		}
 	}
